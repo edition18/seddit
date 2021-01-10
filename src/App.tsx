7fc719440b88a1f10b3964ca4034794e810876b8
Edwin Choo <jchoo004@e.ntu.edu.sg>
2021-01-10 16:49:29 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import 'fontsource-roboto';
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 import "./styles/App.css"
 import Landing from "./components/Landing"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App: FunctionComponent<React.ReactNode>  = () => {
   return (
     <div className="App">
-      <Landing />
+    <Router>
+      <Route path="/" component={Landing}></Route>
+
+    </Router>
     </div>
   );
 }
