bb98e42ee68614d65ffca3f9748011aecd724dac
Edwin Choo <jchoo004@e.ntu.edu.sg>
2021-01-10 16:24:13 +0800

    tested sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import ReactDOM from 'react-dom'
 import 'fontsource-roboto';
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import "./styles/App.css"
+import Landing from "./components/Landing"
 
-function App() {
+const App: FunctionComponent<React.ReactNode>  = () => {
   return (
     <div className="App">
-      Test
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+import 'fontsource-roboto';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+
+
+const Landing: FunctionComponent<React.ReactNode>  = () => {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
+
+export default Landing;
