1c2523b8a31c0189f02271e65cbe2e4de96e8835
Edwin Choo <jchoo004@e.ntu.edu.sg>
2021-01-11 15:53:29 +0800

    more changes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ dependencies:
 MaterialUI
 sass (scss)
 c-r-a (typescript)
+
+axios react-router-dom redux react-redux redux-thunk redux-devtools-extension
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React,  { FunctionComponent , Fragment} from 'react';
-import ReactDOM from 'react-dom'
-import 'fontsource-roboto';
-import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-import "./styles/App.css"
-import Landing from "./components/Landing"
-import Navbar from "./components/Navbar"
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from "./styles/theme"
+import React, { FunctionComponent } from "react";
+import "fontsource-roboto";
+import { AccessAlarm, ThreeDRotation } from "@material-ui/icons";
+import "./styles/App.css";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./styles/theme";
+import firebase from "./firebase";
 
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-const App: FunctionComponent<React.ReactNode>  = () => {
+const App: FunctionComponent<React.ReactNode> = () => {
   return (
-    <ThemeProvider theme={theme}>      
+    <ThemeProvider theme={theme}>
       <Navbar />
       <Router>
         <Switch>
-            <Route path="/" exact component={Landing}></Route>
-
+          <Route path="/" exact component={Landing}></Route>
         </Switch>
       </Router>
     </ThemeProvider>
-
   );
-}
+};
 
 export default App;
